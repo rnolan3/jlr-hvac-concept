41a1816bea58d4d7b598589df3cfcf86992ce366
Robert Nolan <robert@vohgo.com>
2016-02-23 16:46:01 -0600

    Only clone repo when process is not in a production environment

--- postinstall.js
+++ postinstall.js
@@ -8,9 +8,10 @@ function exec (command) {
   execSync(command, { stdio: [ 0, 1, 2 ] })
 }
 
-
-fs.stat(commonApp, (err) => {
-  if (err) {
-    exec('git clone https://github.com/PDXostc/common-app.git ' + commonApp)
-  }
-})
+if (process.env.NODE_ENV !== 'production') {
+  fs.stat(commonApp, (err) => {
+    if (err) {
+      exec('git clone https://github.com/PDXostc/common-app.git ' + commonApp)
+    }
+  })
+}
