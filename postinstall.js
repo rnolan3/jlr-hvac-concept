a90adf521f1b90d1de0d0362ad0fe5d2ce079a6b
Robert Nolan <robert@vohgo.com>
2016-02-23 17:55:19 -0600

    Don't clone common-app in test environment

--- postinstall.js
+++ postinstall.js
@@ -8,7 +8,7 @@ function exec (command) {
   execSync(command, { stdio: [ 0, 1, 2 ] })
 }
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') {
   fs.stat(commonApp, (err) => {
     if (err) {
       exec('git clone https://github.com/PDXostc/common-app.git ' + commonApp)
