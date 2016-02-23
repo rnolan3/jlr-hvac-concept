13d35b4f2240828d4d18b579ec952267fa20074c
Robert Nolan <robert@vohgo.com>
2016-02-23 13:30:36 -0600

    Create npm postinstall script

---
+++ postinstall.js
@@ -0,0 +1,16 @@
+/* eslint no-var: 0 */
+var fs = require('fs')
+var execSync = require('child_process').execSync
+
+var commonApp = __dirname + '/DNA_common'
+
+function exec (command) {
+  execSync(command, { stdio: [ 0, 1, 2 ] })
+}
+
+
+fs.stat(commonApp, (err) => {
+  if (err) {
+    exec('git clone https://github.com/PDXostc/common-app.git ' + commonApp)
+  }
+})
